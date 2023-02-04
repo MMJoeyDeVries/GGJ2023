e0dbc00464f0a71eea040233b14101f71f9f1835
Leroy Korterink <leroy@mediamonks.com>
2023-02-04 22:50:41 +0100

    Add variables for MapGeneration

--- BeatRoot/Assets/Scripts/MapGeneration.cs
+++ BeatRoot/Assets/Scripts/MapGeneration.cs
@@ -7,135 +7,148 @@ using Unity.VisualScripting;
 
 public class MapGeneration : MonoBehaviour
 {
-    public Transform Player;
-    
-    public Spline _TopSpline;
-    public Spline _BottomSpline;
+  public Transform Player;
 
-    [Header("Config")] 
-    public float Width = 3.0f;
-    public float Curvature = 1.0f;
-    public float MinY = -20.0f;
-    public float MaxY = 5.0f;
+  public Spline _TopSpline;
+  public Spline _BottomSpline;
 
-    private Vector3 _CenterLastSplinePosTop = Vector3.zero;
-    private Vector3 _CenterLastSplinePosBottom = Vector3.zero;
+  [Header("Config")]
+  public float Width = 3.0f;
+  public float Curvature = 1.0f;
+  public float MinY = -20.0f;
+  public float MaxY = 5.0f;
 
-    private SplineMeshTiling _MeshTilingTop;
-    private SplineMeshTiling _MeshTilingBottom;
+  public float Randomness = 2.0f;
+  public float Roughness = 2.0f;
+  public float TunnelHeight = 4.0f;
 
-    private float _splineEndX = 0.0f;
-    void Start()
+  private Vector3 _CenterLastSplinePosTop = Vector3.zero;
+  private Vector3 _CenterLastSplinePosBottom = Vector3.zero;
+
+  private SplineMeshTiling _MeshTilingTop;
+  private SplineMeshTiling _MeshTilingBottom;
+
+  private float _splineEndX = 0.0f;
+  void Start()
+  {
+    // _TopSpline.Reset();
+    // _BottomSpline.Reset();
+
+    // for (int i = 0; i < _TopSpline.transform.GetChild(0).childCount; i++)
+    // {
+    //     Destroy(_TopSpline.transform.GetChild(0).GetChild(i).gameObject);
+    // }
+    // for (int i = 0; i < _BottomSpline.transform.GetChild(0).childCount; i++)
+    // {
+    //     Destroy(_BottomSpline.transform.GetChild(0).GetChild(i).gameObject);
+    // }
+
+    _MeshTilingTop = _TopSpline.GetComponent<SplineMeshTiling>();
+    _MeshTilingBottom = _BottomSpline.GetComponent<SplineMeshTiling>();
+
+    // create 4 segments to override base segments, help fix some issues
+    // _TopSpline.nodes[0].Position = new Vector3(0.0f, Width, 0.0f);
+    // _BottomSpline.nodes[0].Position = new Vector3(0.0f, -Width, 0.0f);
+    // _TopSpline.nodes[1].Position = new Vector3(4.0f, Width, 0.0f);
+    // _BottomSpline.nodes[1].Position = new Vector3(4.0f, -Width, 0.0f);
+
+    _TopSpline.nodes[0].Position = new Vector3(0.0f, 3.0f, 0.0f);
+    _TopSpline.nodes[0].Direction = new Vector3(1.0f, 3.0f, 0.0f);
+    _TopSpline.nodes[1].Position = new Vector3(4.0f, 3.0f, 0.0f);
+    _TopSpline.nodes[1].Direction = new Vector3(5.0f, 3.0f, 0.0f);
+    _TopSpline.nodes[2].Position = new Vector3(8.0f, 3.0f, 0.0f);
+    _TopSpline.nodes[2].Direction = new Vector3(9.0f, 3.0f, 0.0f);
+
+    _BottomSpline.nodes[0].Position = new Vector3(0.0f, -3.0f, 0.0f);
+    _BottomSpline.nodes[0].Direction = new Vector3(1.0f, -3.0f, 0.0f);
+    _BottomSpline.nodes[1].Position = new Vector3(4.0f, -3.0f, 0.0f);
+    _BottomSpline.nodes[1].Direction = new Vector3(5.0f, -3.0f, 0.0f);
+    _BottomSpline.nodes[2].Position = new Vector3(8.0f, -3.0f, 0.0f);
+    _BottomSpline.nodes[2].Direction = new Vector3(9.0f, -3.0f, 0.0f);
+
+    _CenterLastSplinePosTop = _TopSpline.nodes[2].Position;
+    _CenterLastSplinePosBottom = _BottomSpline.nodes[2].Position;
+
+    _MeshTilingTop.CreateMeshes();
+    _MeshTilingBottom.CreateMeshes();
+
+    Debug.Log(_CenterLastSplinePosTop);
+    Debug.Log(_CenterLastSplinePosBottom);
+
+    _splineEndX = CreateNewSegment(25);
+  }
+
+  private float CreateNewSegment(int count)
+  {
+    for (int i = 0; i < count; ++i)
     {
-        // _TopSpline.Reset();
-        // _BottomSpline.Reset();
+      float maxYDiff = MaxY - _CenterLastSplinePosTop.y;
+      float minYDiff = _CenterLastSplinePosTop.y - MinY;
 
-        // for (int i = 0; i < _TopSpline.transform.GetChild(0).childCount; i++)
-        // {
-        //     Destroy(_TopSpline.transform.GetChild(0).GetChild(i).gameObject);
-        // }
-        // for (int i = 0; i < _BottomSpline.transform.GetChild(0).childCount; i++)
-        // {
-        //     Destroy(_BottomSpline.transform.GetChild(0).GetChild(i).gameObject);
-        // }
+      float random = Random.Range(
+        -Randomness + Mathf.Max(Randomness - minYDiff, 0.0f),
+        Randomness - Mathf.Max(Randomness - maxYDiff, 0.0f)
+      );
 
-        _MeshTilingTop = _TopSpline.GetComponent<SplineMeshTiling>();
-        _MeshTilingBottom = _BottomSpline.GetComponent<SplineMeshTiling>();
-        
-        // create 4 segments to override base segments, help fix some issues
-        // _TopSpline.nodes[0].Position = new Vector3(0.0f, Width, 0.0f);
-        // _BottomSpline.nodes[0].Position = new Vector3(0.0f, -Width, 0.0f);
-        // _TopSpline.nodes[1].Position = new Vector3(4.0f, Width, 0.0f);
-        // _BottomSpline.nodes[1].Position = new Vector3(4.0f, -Width, 0.0f);
+      // top spline
+      Vector3 prev = _CenterLastSplinePosTop;
+      Vector3 pos = new Vector3(prev.x + TunnelHeight, prev.y + random, 0.0f);
+      _TopSpline.AddNode(new SplineNode(pos, pos + (pos - prev).normalized * Roughness));
+      _CenterLastSplinePosTop = pos;
 
-        _TopSpline.nodes[0].Position = new Vector3(0.0f, 3.0f, 0.0f);
-        _TopSpline.nodes[0].Direction = new Vector3(1.0f, 3.0f, 0.0f);
-        _TopSpline.nodes[1].Position = new Vector3(4.0f, 3.0f, 0.0f);
-        _TopSpline.nodes[1].Direction = new Vector3(5.0f, 3.0f, 0.0f);
-        _TopSpline.nodes[2].Position = new Vector3(8.0f, 3.0f, 0.0f);
-        _TopSpline.nodes[2].Direction = new Vector3(9.0f, 3.0f, 0.0f);
-        
-        _BottomSpline.nodes[0].Position = new Vector3(0.0f, -3.0f, 0.0f);
-        _BottomSpline.nodes[0].Direction = new Vector3(1.0f, -3.0f, 0.0f);
-        _BottomSpline.nodes[1].Position = new Vector3(4.0f, -3.0f, 0.0f);
-        _BottomSpline.nodes[1].Direction = new Vector3(5.0f, -3.0f, 0.0f);
-        _BottomSpline.nodes[2].Position = new Vector3(8.0f, -3.0f, 0.0f);
-        _BottomSpline.nodes[2].Direction = new Vector3(9.0f, -3.0f, 0.0f);
-
-        _CenterLastSplinePosTop = _TopSpline.nodes[2].Position;
-        _CenterLastSplinePosBottom = _BottomSpline.nodes[2].Position;
-        
-        _MeshTilingTop.CreateMeshes();
-        _MeshTilingBottom.CreateMeshes();
-
-        Debug.Log(_CenterLastSplinePosTop);
-        Debug.Log(_CenterLastSplinePosBottom);
-
-        _splineEndX = CreateNewSegment(25);
+      // bottom spline
+      prev = _CenterLastSplinePosBottom;
+      pos = new Vector3(prev.x + TunnelHeight, prev.y + random, 0.0f);
+      _BottomSpline.AddNode(new SplineNode(pos, pos + (pos - prev).normalized * Roughness));
+      _CenterLastSplinePosBottom = pos;
     }
 
-    private float CreateNewSegment(int count)
-    {
-        for (int i = 0; i < count; ++i)
-        {
-            float maxYDiff = MaxY - _CenterLastSplinePosTop.y;
-            float minYDiff = _CenterLastSplinePosTop.y - MinY;
+    // generate new mesh
+    _MeshTilingTop.CreateMeshes();
+    _MeshTilingBottom.CreateMeshes();
 
-            float random = Random.Range(-2.0f + Mathf.Max(2.0f - minYDiff, 0.0f), 2.0f - Mathf.Max(2.0f - maxYDiff, 0.0f));
-            // top spline
-            Vector3 prev = _CenterLastSplinePosTop;
-            Vector3 pos = new Vector3(prev.x + 4.0f, prev.y + random, 0.0f);
-            _TopSpline.AddNode(new SplineNode(pos, pos + (pos - prev).normalized * 2.0f));
-            _CenterLastSplinePosTop = pos;
-            
-            // bottom spline
-            prev = _CenterLastSplinePosBottom;
-            pos = new Vector3(prev.x + 4.0f, prev.y + random, 0.0f);
-            _BottomSpline.AddNode(new SplineNode(pos, pos + (pos - prev).normalized * 2.0f));
-            _CenterLastSplinePosBottom = pos;
-        }
-        
-        // generate new mesh
-        _MeshTilingTop.CreateMeshes();
-        _MeshTilingBottom.CreateMeshes();
-        
-        // return last generated x coordinate so we know when to generate a new segment later on
-        return _TopSpline.GetSampleAtDistance(_TopSpline.Length).location.x;
+    // return last generated x coordinate so we know when to generate a new segment later on
+    return _TopSpline.GetSampleAtDistance(_TopSpline.Length).location.x;
+  }
+
+  public bool TunnelIntersect(Vector3 position, float radius)
+  {
+    CurveSample topSample = _TopSpline.GetProjectionSample(position);
+    CurveSample bottomSample = _BottomSpline.GetProjectionSample(position);
+
+    Vector3 top = topSample.location - position;
+    Vector3 bottom = bottomSample.location - position;
+
+    bool sameDirection = Vector3.Dot(top, bottom) >= 0.0f;
+
+    if (sameDirection)
+    {
+
+    }
+    else
+    {
+      // position is in tunnel,
     }
 
-    public bool TunnelIntersect(Vector3 position, float radius)
+    return !sameDirection;
+  }
+
+  void Update()
+  {
+    if (Player != null)
     {
-        CurveSample topSample = _TopSpline.GetProjectionSample(position);
-        CurveSample bottomSample = _BottomSpline.GetProjectionSample(position);
+      if (Mathf.Abs(Player.position.x - _splineEndX) < 10.0f)
+      {
+        Debug.Log("New segment!");
+        _splineEndX = CreateNewSegment(10);
+      }
 
-        Vector3 top = topSample.location - position;
-        Vector3 bottom = bottomSample.location - position;
-
-        bool sameDirection = Vector3.Dot(top, bottom) >= 0.0f;
-
-        if (sameDirection)
-        {
-            
-        }
-        else
-        {
-            // position is in tunnel, 
-        }
-
-        return !sameDirection;
+      // Debug.Log(IsInTunnel(Player.position).ToString());
     }
-    
-    void Update()
-    {
-        if (Player != null)
-        {
-            if (Mathf.Abs(Player.position.x - _splineEndX) < 10.0f)
-            {
-                Debug.Log("New segment!");
-                _splineEndX = CreateNewSegment(10);
-            }
+  }
 
-            // Debug.Log(IsInTunnel(Player.position).ToString());
-        }
-    }
+  public void Reset()
+  {
+    Debug.Log("Not Implemented");
+  }
 }
